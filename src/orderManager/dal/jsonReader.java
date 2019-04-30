87077b10fe6a9013c90d327700803ac04c7dedb3
Revy <Lopas654>
2019-05-01 00:15:12 +0200

    JSON Reader kinda works

--- src/orderManager/dal/jsonReader.java
+++ src/orderManager/dal/jsonReader.java
@@ -1,7 +1,32 @@
 package orderManager.dal;
+
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class jsonReader {
 
-  public void readFile() {
+  public static void readFile() {
+    JSONParser parser = new JSONParser();
+
+    try {
+      JSONObject object = (JSONObject) parser.parse(new FileReader("data.json"));
+
+      JSONArray jArray = (JSONArray) object.get("AvailableWorkers");
+      for (int i = 0; i < jArray.size(); i++) {
+        JSONObject rec = (JSONObject) jArray.get(i);
+        System.out.println("Name: "+rec.get("Name"));
+        System.out.println("Initials: "+rec.get("Initials"));
+        System.out.println("Salary Number: "+rec.get("SalaryNumber"));
+        System.out.println("-----------------");
+      }
+
+    } catch (ParseException | IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
--- src/orderManager/gui/controller/mainWindowController.java
+++ src/orderManager/gui/controller/mainWindowController.java
@@ -6,37 +6,34 @@ import java.util.ResourceBundle;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
 import javafx.application.Platform;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import orderManager.dal.jsonReader;
 
 public class mainWindowController implements Initializable {
 
-    public AnchorPane mainPane;
-    public Label dateLabel;
-    private ScheduledExecutorService executor;
+  public AnchorPane mainPane;
+  public Label dateLabel;
+  private ScheduledExecutorService executor;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        displayTime();
-    }
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    displayTime();
+    jsonReader.readFile();
+  }
 
-    public void displayTime() {
-        Runnable thread = () -> {
-            setTime();
-        };
-        executor = Executors.newScheduledThreadPool(1);
-        executor.scheduleAtFixedRate(thread, 0, 1, TimeUnit.SECONDS);
-    }
+  public void displayTime() {
+    Runnable thread = () -> setTime();
+    executor = Executors.newScheduledThreadPool(1);
+    executor.scheduleAtFixedRate(thread, 0, 1, TimeUnit.SECONDS);
+  }
 
-    public void setTime() {
-        Platform.runLater(() -> {
-            dateLabel.setText(String.valueOf(Calendar.getInstance().getTime()));
-        });
+  public void setTime() {
+    Platform.runLater(() -> dateLabel.setText(String.valueOf(Calendar.getInstance().getTime())));
 
-    }
+  }
 
 
 }
