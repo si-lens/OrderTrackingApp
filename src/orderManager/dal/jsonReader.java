9dec0b0cc9618f99aed6a787825c9f1fd2d30867
leopoldus97 <mate1011@easv365.dk>
2019-05-03 21:30:44 +0200

    jsonReader update

--- src/orderManager/dal/jsonReader.java
+++ src/orderManager/dal/jsonReader.java
@@ -9,6 +9,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
+
+import orderManager.be.Department;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -39,7 +41,7 @@ public class jsonReader {
     try {
       object = (JSONObject) parser.parse(new FileReader("data.json"));
       loadWorkers();
-      loadOrders();
+      loadProductionOrders();
     } catch (java.text.ParseException | ParseException | IOException | SQLException e) {
       e.printStackTrace();
     }
@@ -62,7 +64,7 @@ public class jsonReader {
   }
 
 
-  public static void loadOrders() throws SQLException, java.text.ParseException {
+  public static void loadProductionOrders() throws SQLException, java.text.ParseException {
     JSONArray jArray = (JSONArray) object.get("ProductionOrders");
     for (int i = 0; i < jArray.size(); i++) {
       JSONObject rec = (JSONObject) jArray.get(i);
@@ -71,12 +73,12 @@ public class jsonReader {
       ppst.setString(1, (String) rec.get("__type"));
       ppst.setInt(2, i);
       ppst.execute();
-      loadCustomer((JSONObject)rec.get("Customer"),i);
+      loadCustomers((JSONObject)rec.get("Customer"),i);
       loadDelivery((JSONObject)rec.get("Delivery"),i);
     }
   }
 
-  public static void loadCustomer(JSONObject customer, int id) throws SQLException {
+  public static void loadCustomers(JSONObject customer, int id) throws SQLException {
     String sql = "INSERT INTO Customer(Type,Name,ID) VALUES (?,?,?)";
     PreparedStatement ppst = con.prepareStatement(sql);
     ppst.setString(1, (String) customer.get("__type"));
@@ -85,8 +87,7 @@ public class jsonReader {
     ppst.execute();
   }
 
-  public static void loadDelivery(JSONObject delivery, int id)
-      throws SQLException {
+  public static void loadDelivery(JSONObject delivery, int id) throws SQLException {
     String date = (String) delivery.get("DeliveryTime");
     date = date.replace("/Date(","").replace("+0200)/","");
     Date currentDate = new Date(Long.valueOf(date));
@@ -98,5 +99,39 @@ public class jsonReader {
     ppst.execute();
   }
 
+  public static void loadDepartment(JSONObject department, int id) throws SQLException {
+      String sql = "INSERT INTO Department(Type, Name, ID) VALUES(?, ?, ?)";
+      PreparedStatement ppst = con.prepareStatement(sql);
+      ppst.setString(1, (String) department.get("__type"));
+      ppst.setString(2, (String) department.get("Name"));
+      ppst.setInt(3, id);
+      ppst.execute();
+  }
+
+  public static void loadOrder(JSONObject order, int id) throws SQLException {
+    String sql = "INSERT INTO Order(Type, OrderNumber, ID) VALUE(?, ?, ?)";
+    PreparedStatement ppst = con.prepareStatement(sql);
+    ppst.setString(1, (String) order.get("__type"));
+    ppst.setString(2, (String) order.get("OrderNumber"));
+    ppst.setInt(3, id);
+    ppst.execute();
+  }
+
+  public static void loadDepartmentTasks(JSONObject departmentTask, int id, int productionOrderID) throws SQLException {
+    String startDate = (String) departmentTask.get("StartDate");
+    startDate = startDate.replace("/Date(","").replace("+0200)/","");
+    Date start = new Date(Long.valueOf(startDate));
+    String finishedDate = (String) departmentTask.get("FinishedOrder");
+    finishedDate = finishedDate.replace("/Date(","").replace("+0200)/","");
+    Date finished = new Date(Long.valueOf(finishedDate));
+    String sql = "INSERT INTO DepartmentTasks(ID, Type, EndDate, StartDate, FinishedOrder, ProductionOrderID) VALUE(?, ?, ?, ?, ?, ?)";
+    PreparedStatement ppst = con.prepareStatement(sql);
+    ppst.setInt(1, id);
+    ppst.setString(2, (String) departmentTask.get("__type"));
+    ppst.setDate(3, start);
+    ppst.setDate(4, finished);
+    ppst.setBoolean(5, (Boolean) departmentTask.get("FinishedOrder"));
+    ppst.setInt(6, productionOrderID);
+  }
 
 }
